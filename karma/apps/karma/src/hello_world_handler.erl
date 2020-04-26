f70c86876b0a2416097ff8ec2e378c05b637dae2
Simon Forman <sforman@hushmail.com>
2020-04-26 15:44:40 -0700

    "It doesn't work."

---
+++ karma/apps/karma/src/hello_world_handler.erl
@@ -0,0 +1,11 @@
+%% @doc Hello world handler.
+-module(hello_world_handler).
+
+-export([init/2]).
+
+init(Req0, Opts) ->
+	Req = cowboy_req:reply(200, #{
+		<<"content-type">> => <<"text/plain">>
+	}, <<"Hello world!">>, Req0),
+	{ok, Req, Opts}.
+
